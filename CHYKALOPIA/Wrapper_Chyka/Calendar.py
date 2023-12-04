f6c4189b1acfd079031962a9eade1e90c54ae310
Iuliia603 <iuliia60358@gmail.com>
2023-12-04 14:33:12 -0500

    Update Calendar.py

--- CHYKALOPIA/Wrapper_Chyka/Calendar.py
+++ CHYKALOPIA/Wrapper_Chyka/Calendar.py
@@ -1,20 +1,22 @@
 from CHYKALOPIA.Wrapper_Chyka.UIElement import UIElement as Element
 from selenium.webdriver.common.by import By
-from CHYKALOPIA.Wrapper_Chyka.Browser import Browser
 import time
+
 
 class Calendar:
     def __init__(self, browser):
-        self.browser = Browser
+        self.browser = browser
         self.calendar_title = Element(browser, By.XPATH, '//*[@id="root"]/div/div/div/div[2]/div/h2')
         self.time_3_45 = Element(browser, By.XPATH, '//*[@id="root"]/div/div/div/div[2]/div/div[1]/div[2]/div/div/div[2]/button')
         self.time_3_30 = Element(browser, By.XPATH, '//*[@id="root"]/div/div/div/div[2]/div/div[1]/div[2]/div/div/div[2]/button')
-        self.next_btn = Element(browser, By.XPATH, '//*[@id="root"]/div/div/div/div[2]/div/div[1]/div[2]/div/div/div[2]/button[2]')
-        self.iframe = Element(browser, By.XPATH, '//*[@id="get-started"]/div/div/iframe')
+        self.next_btn = Element(browser, By.XPATH, '//*[@id="root"]/div/div/div/div[2]/div/div[1]/div[2]/div/div/div[1]/button[2]')
+        self.iframe = Element(browser, By.TAG_NAME, 'iframe')
+        self.time_11 = Element(browser, By.XPATH, '//*[@id="root"]/div/div/div/div[2]/div/div[1]/div[2]/div/div/div[1]/button')
+        self.information_about_call = Element(browser, By.XPATH, '//*[@id="root"]/div/div/div/div[1]/div/div/div[1]/div[2]/div/div/div/div/div[3]/div/div[3]')
 
     def switch_to_calendar_frame(self):
-        self.browser.get_driver().switch_to.frame(self.iframe.get_element())
         time.sleep(10)
+        self.browser.get_driver().switch_to.frame(self.iframe.get_element())
 
     def switch_back_from_calendar_frame(self):
         self.browser.get_driver().switch_to.default_content()
@@ -23,8 +25,8 @@ class Calendar:
         return self.calendar_title.get_text()
 
     def select_the_date(self, date_to_select):
-        self.date = Element(self.browser, By.XPATH, '//button[contains(@aria-label, "{}")]'.format(date_to_select))
-        self.date.click()
+        self.date_12 = Element(self.browser, By.XPATH, '//button[contains(@aria-label, "{}")]'.format(date_to_select))
+        self.date_12.click()
 
     def select_time_3_45(self):
         self.time_3_45.click()
@@ -32,6 +34,12 @@ class Calendar:
     def select_time_3_30(self):
         self.time_3_30.click()
 
+    def select_time_11(self):
+        self.time_11.click()
+
     def click_next_btn(self):
         self.next_btn.click()
+
+    def get_information_about_call(self):
+        return self.information_about_call.get_text()
 
